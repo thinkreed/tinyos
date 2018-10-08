b366744fa6f203124f79f64131d119270464042b
thinkreed <thinkreed2017@outlook.com>
2018-10-08 08:50:49 +0800

    function boot sect

---
+++ src/boot_sect_main.asm
@@ -0,0 +1,30 @@
+[org 0x7c00]
+
+mov bx, HELLO
+call print
+
+call print_nl
+
+mov bx, GOODBYE
+call print
+
+call print_nl
+
+mov dx, 0x12fe
+call print_hex
+
+jmp $ 
+
+
+%include "src/boot_sect_print.asm"
+%include "src/boot_sect_print_hex.asm"
+
+HELLO:
+    db 'Hello, World', 0
+
+GOODBYE:
+    db 'Goodbye', 0
+
+
+times 510-($-$$) db 0
+dw 0xaa55
